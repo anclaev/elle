2f13efbb59492338cdc10c96879ea0bf181406a6
anclaev <iahugo@yandex.ru>
2023-10-28 23:16:26 +0300

    Стартовая команда.

---
+++ src/app.update.ts
@@ -0,0 +1,23 @@
+import { Action, Start, Update } from 'nestjs-telegraf'
+import { Injectable } from '@nestjs/common'
+import { Context } from 'telegraf'
+
+import { StartButtons } from 'app.buttons'
+
+@Update()
+@Injectable()
+export class AppUpdate {
+  @Start()
+  async startCommand(ctx: Context) {
+    await ctx.reply('Привет!')
+    await ctx.reply(
+      'Меня зовут Эль и я будущая дочь самых лучший родителей на свете 🥹',
+      StartButtons(),
+    )
+  }
+
+  @Action('actions')
+  async getActions() {
+    return 'Пока что я ничего не умею. Но совсем скоро научусь, обещаю!'
+  }
+}
