68fdb079a64a935181d8f0c4b6ffc2132230f883
anclaev <iahugo@yandex.ru>
2023-10-28 19:25:36 +0300

    Подключение к базе данных.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
+import { MongooseModule } from '@nestjs/mongoose'
 import { ConfigModule } from '@nestjs/config'
 import { Module } from '@nestjs/common'
 
 import { CommonModule } from './common/common.module'
+import { ConfigService } from '@common/services'
 
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
@@ -9,7 +11,14 @@ import { AppService } from './app.service'
 import { options } from '@utils/config'
 
 @Module({
-  imports: [ConfigModule.forRoot(options), CommonModule],
+  imports: [
+    ConfigModule.forRoot(options),
+    CommonModule,
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => config.dbOptions(),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/common/services/config.service.ts
+++ src/common/services/config.service.ts
@@ -1,4 +1,5 @@
 import { ConfigService as RootConfigService } from '@nestjs/config'
+import { MongooseModuleFactoryOptions } from '@nestjs/mongoose'
 import { Injectable } from '@nestjs/common'
 
 import { IConfig } from '../interfaces/config'
@@ -15,5 +16,20 @@ export class ConfigService extends RootConfigService {
 
   port(): number {
     return Number(this.get('APP_PORT'))
+  }
+
+  dbOptions(): MongooseModuleFactoryOptions {
+    const db = {
+      host: this.get<string>('MONGO_HOST'),
+      port: this.get<number>('MONGO_PORT'),
+      user: this.get<string>('MONGO_USER'),
+      password: this.get<string>('MONGO_PASS'),
+      name: this.get<string>('MONGO_DB'),
+    }
+
+    const options: MongooseModuleFactoryOptions = {
+      uri: `mongodb://${db.host}:${db.port}/${db.name}`,
+    }
+    return options
   }
 }
